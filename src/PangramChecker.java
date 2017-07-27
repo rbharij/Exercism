0b799ddbd8e75b546133dca03f163968b21fce51
rbharij <r_bharij@hotmail.com>
2017-07-27 16:41:52 +0800

    Pangram

--- src/PangramChecker.java
+++ src/PangramChecker.java
@@ -2,8 +2,44 @@ public class PangramChecker {
     public boolean isPangram(String s) {
 
         if (s == "")
+            //No string passed - not a Pangram
             return false;
         else
-            return true;
+            // First check if the string is all in lowercase
+            return isTheStringLowercase(s);
     }
+
+    public boolean isTheStringLowercase (String text) {
+
+        //Loop through each character in the string and check if there are any uppercase letters and return false if found
+
+        for (int i = 0; i<text.length();i++)
+        {
+            // Check for uppercase
+            if (Character.isUpperCase(text.charAt(i)))
+            {
+                System.out.print("Uppercase characters found");
+                return false;
+            }
+        }
+        System.out.print("No uppercase characters found");
+        return true;
+    }
+
+
+    public boolean isTheStringAPangram(String txt){
+        // Method to determine if the text is a Pangram
+        // Create an Array to hold the letters in the alphabet
+        String myStringArray[];
+
+        
+
+        //The text is not a Pangram
+        return false;
+        //the Text is a Pangram
+    }
+
+
+
+
 }
