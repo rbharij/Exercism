11829498888dff609a86aa860abb07078b2120d1
rbharij <r_bharij@hotmail.com>
2017-07-31 21:40:30 +0800

    RNA Transcription Completed

--- src/RnaTranscription.java
+++ src/RnaTranscription.java
@@ -1,5 +1,25 @@
 public class RnaTranscription {
     public String transcribe(String s) {
-    return "";
+        //Build an Array of the Chars in the passed in string
+        char arr[]=s.toCharArray();
+        //Intialise a new variables
+        String transcribedstring = "";
+        //For each Char is the array get its corresponding char (DNA>RNA)
+        for(int i=0;i<arr.length;i++){
+            //Build the transcrbed sequence
+            switch (arr[i]) {
+                case 'A': transcribedstring = transcribedstring + "U";
+                    break;
+                case 'C': transcribedstring = transcribedstring + "G";
+                    break;
+                case 'G': transcribedstring = transcribedstring + "C";
+                    break;
+                case 'T': transcribedstring = transcribedstring + "A";
+                    break;
+                default : transcribedstring = "";
+            }
+        }
+        System.out.println("DNA sequence [" + s + "] transcribed to RNA sequence [" + transcribedstring + "]");
+        return transcribedstring ;
     }
 }
