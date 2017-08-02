b98fbe052e65d7452f19e7a8efa350d5801eea18
rbharij <r_bharij@hotmail.com>
2017-08-02 21:12:49 +0800

    Completed Acronym Code

--- src/Acronym.java
+++ src/Acronym.java
@@ -1,22 +1,22 @@
-public class Acronym{
+public class Acronym {
 
+    String TexttoCheck = "";
 
-    public Acronym(String phrase) {
-    // Read in the passed in Phrase into an Array
-    char PhraseArray[]=phrase.toCharArray();
-    //Loop through each character in the array and take not of every capital letter
-
-
-
+    public Acronym(String Phrase) {
+        //Replace any hyphens with spaces
+        Phrase = Phrase.replaceAll("-", " ");
+        //Split the phrase into an Array by finding the spaces
+        String[] letters = Phrase.split(" ");
+        // Extract the first letter of each word and add that to the return phrase
+        for(int i=0;i<letters.length;i++){
+            //Get the first letter
+            String A = letters[i].substring(0,1);
+            TexttoCheck = TexttoCheck + A;
+        }
     }
 
     public String get() {
 
-        //Declare variable for returning the Acronym
-        String AcronymString = "";
-
-
-        // Return the Acronym
-        return AcronymString;
+        return TexttoCheck.toUpperCase();
     }
 }
